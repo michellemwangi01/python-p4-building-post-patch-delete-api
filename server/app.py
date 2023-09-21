0a8bc80545b1d10776855f910a6e56cdd59b6e31
michellemwangi01 <michellemwangim@gmail.com>
2023-09-21 10:38:31 +0300

    completed

--- server/app.py
+++ server/app.py
@@ -15,15 +15,16 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+
 @app.route('/')
 def index():
     return "Index for Game/Review/User API"
 
+
 @app.route('/games')
 def games():
-
     games = []
-    for game in Game.query.all():
+    for game in Game.query.order_by(Game.created_at.desc()).all():
         game_dict = {
             "title": game.title,
             "genre": game.genre,
@@ -39,10 +40,11 @@ def games():
 
     return response
 
+
 @app.route('/games/<int:id>')
 def game_by_id(id):
     game = Game.query.filter(Game.id == id).first()
-    
+
     game_dict = game.to_dict()
 
     response = make_response(
@@ -52,24 +54,38 @@ def game_by_id(id):
 
     return response
 
-@app.route('/reviews')
+
+@app.route('/reviews', methods=['GET', 'POST'])
 def reviews():
+    if request.method == 'GET':
+        reviews = []
+        for review in Review.query.all():
+            review_dict = review.to_dict()
+            reviews.append(review_dict)
 
-    reviews = []
-    for review in Review.query.all():
-        review_dict = review.to_dict()
-        reviews.append(review_dict)
+        response = make_response(
+            reviews,
+            200
+        )
+        return response
 
-    response = make_response(
-        reviews,
-        200
-    )
+    elif request.method == 'POST':
+        new_review = Review(
+            score = request.form.get("score"),
+            comment = request.form.get("comment"),
+            game_id = request.form.get("game_id"),
+            user_id = request.form.get("user_id")
+        )
+        db.session.add(new_review)
+        db.session.commit()
 
-    return response
+        review_dict = new_review.to_dict()
+        response = make_response(review_dict,201)
+        return response
+
 
 @app.route('/users')
 def users():
-
     users = []
     for user in User.query.all():
         user_dict = user.to_dict()
@@ -81,6 +97,52 @@ def users():
     )
 
     return response
+
+
+@app.route('/reviews/<int:id>', methods=['GET', 'DELETE', 'PATCH'])
+def review_by_id(id):
+    review = Review.query.filter(Review.id == id).first()
+    if review is None:
+        message_body = {
+            "message": "SORRY! The requested review does not exist. Please try again"
+        }
+        response = make_response(message_body, 404)
+        return response
+
+    if request.method == 'GET':
+        request_data = {
+            'method': request.method,
+            'path': request.path,
+            'headers': dict(request.headers),
+            'data': request.data.decode('utf-8'),
+            'form_data': request.form
+        }
+        review_dict = review.to_dict()
+        response = make_response(review_dict, 200)
+        return response
+
+    elif request.method == 'DELETE':
+        db.session.delete(review)
+        db.session.commit()
+        response_body = {
+            "delete successful": "True",
+            "message": "Review deleted."
+        }
+        response = make_response(response_body, 200)
+        return response
+
+    elif request.method == "PATCH":
+        for attr in request.form:
+            setattr(review, attr, request.form.get(attr))
+
+        db.session.add(review)
+        db.session.commit()
+
+        review_dict = review.to_dict()
+        response = make_response(review_dict,200)
+
+        return response
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
